c7740e5cc855d87e788a7597cf484a3b2e35781a
idzharulhuda13 <idzharul.huda@gmail.com>
2021-05-18 17:31:12 +0700

    Pages Product Done

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -27,22 +27,10 @@
                             <div class="product-pic-zoom">
                                 <img class="product-big-img" :src="gambar_default" alt="" />
                             </div>
-                            <div class="product-thumbs">
+                            <div class="product-thumbs" v-if="productDetails.galleries.length > 0">
                                 <carousel :dots="false" :nav="false" class="product-thumbs-track ps-slider">
-                                    <div class="pt" @click="changeImage(thumbs[0])" :class="thumbs[0] == gambar_default ? 'active' : ''">
-                                        <img src="img/products/gamagita_patera1.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == gambar_default ? 'active' : ''">
-                                        <img src="img/products/gamagita_patera2.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == gambar_default ? 'active' : ''">
-                                        <img src="img/products/gamagita_patera3.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == gambar_default ? 'active' : ''">
-                                        <img src="img/products/gamagita_patera4.jpg" alt="" />
+                                    <div v-for="images in productDetails.galleries" :key="images.id" class="pt" @click="changeImage(images.photo)" :class="images.photo == gambar_default ? 'active' : ''">
+                                        <img :src="images.photo" alt="" />
                                     </div>
                                 </carousel>
                             </div>
@@ -50,20 +38,12 @@
                         <div class="col-lg-6">
                             <div class="product-details text-left">
                                 <div class="pd-title">
-                                    <span>chocolate</span>
-                                    <h3>Patera</h3>
+                                    <span>{{productDetails.type}}</span>
+                                    <h3>{{productDetails.name}}</h3>
                                 </div>
                                 <div class="pd-desc">
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
-                                    </p>
-                                    <p>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                                    </p>
-                                    <h4>$15.00</h4>
+                                    <p>{{productDetails.description}}</p>
+                                    <h4>${{productDetails.price}}</h4>
                                 </div>
                                 <div class="quantity">
                                     <router-link to="/shoppingcart" class="primary-btn pd-cart">Add To Cart</router-link>
@@ -91,6 +71,8 @@ import Footer from '@/components/Footer.vue';
 import RelatedProduct from '@/components/RelatedProduct.vue';
 import carousel from 'vue-owl-carousel';
 
+import axios from 'axios';
+
 export default {
     name: 'Product',
     components: {
@@ -101,20 +83,35 @@ export default {
     },
     data() {
         return {
-            gambar_default: "img/products/gamagita_patera1.jpg",
+            gambar_default: '',
             thumbs: [
                 "img/products/gamagita_patera1.jpg",
                 "img/products/gamagita_patera2.jpg",
                 "img/products/gamagita_patera3.jpg",
                 "img/products/gamagita_patera4.jpg",
-            ]
+            ],
+            productDetails: []
         }
     },
     methods: {
         changeImage(urlImage) {
             this.gambar_default = urlImage;
-        }
-    }
+        },
+        setDataPictures(data) {
+            this.productDetails = data;
+            this.gambar_default = data.galleries[0].photo;
+        },
+    },
+    mounted() {
+      axios
+        .get("http://127.0.0.1:8001/api/products", {
+            params: {
+                id: this.$route.params.id
+            }
+        })
+        .then(res => (this.setDataPictures(res.data.data)))
+        .catch(err => console.log(err));
+  }
 };
 </script>
 
